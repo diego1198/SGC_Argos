15332a8eda8e5f974178a2a522f8ebce6d700e50
Diego Beltrán <diegoandres1198@gmail.com>
2020-09-06 17:46:15 -0500

    Diego Beltrán - Add gitignore

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
 $server   = "localhost";
-$username = "sgipro_sgc_argos";
-$password = "202009argos";
-$database = "sgipro_sgc_argos";
+$username = "root";
+$password = "";
+$database = "sgc_argos";
 $mysqli = new mysqli($server, $username, $password, $database);
 if ($mysqli->connect_error) {
     die('error'.$mysqli->connect_error);
