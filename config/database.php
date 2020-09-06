c9309cb791e52b0ffaf3c5954b68f724348ba68d
Diego Beltrán <diegoandres1198@gmail.com>
2020-09-06 17:42:13 -0500

    Diego Beltrán - DB Configuration

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 $server   = "localhost";
 $username = "sgipro_sgc_argos";
 $password = "202009argos";
-$database = "sgc_argos";
+$database = "sgipro_sgc_argos";
 $mysqli = new mysqli($server, $username, $password, $database);
 if ($mysqli->connect_error) {
     die('error'.$mysqli->connect_error);
